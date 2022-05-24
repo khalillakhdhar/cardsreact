86686e57a0ad90615a732ad73b2883a49be61868
khalil <khalillakhdharatc@gmail.com>
2022-05-24 16:19:01 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import  React  from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props)
+    {
+        super(props);
+        this.state={
+             numbers : [1, 2, 3, 4, 5],
+             students : [
+                {
+                'id': 1,
+                'name': 'Jack',
+                'email': 'jack@gmail.com'
+                },
+                {
+                'id': 2,
+                'name': 'Mary',
+                'email': 'mary@gmail.com'
+                },
+                {
+                'id': 3,
+                'name': 'John',
+                'email': 'john@gmail.com'
+                },
+                ]
+        }
+
+
+    }
+   
+    render()
+    {
+        return(
+            <><div>
+                <ul>{this.state.numbers.map((num) => <li>{num}</li>)}</ul>
+            </div>
+            <h1 className='text-center'> List of student </h1>
+<table className="table table-bordered">
+<tr className='text-center'>
+<th >ID</th>
+<th>Name</th>
+<th>Email</th></tr>
+{this.state.students.map((student, index) => (
+<tr className='text-center' data-index={index}>
+<td>{student.id}</td>
+<td>{student.name}</td>
+<td>{student.email}</td>
+</tr>
+))}
+</table>
+<div>
+                
+                
+                
+                </div></>
+        );
+    }
 }
-
 export default App;
